6c607783b96c56e4bf8533543c61d5ef2eb0ea30
Emanuele Blanco <emanuele.blanco@gmail.com>
2016-05-23 14:17:22 +0100

    Added sandbox version of levy-declarations endpoint.

--- app/uk/gov/hmrc/apprenticeshiplevy/config/microserviceWiring.scala
+++ app/uk/gov/hmrc/apprenticeshiplevy/config/microserviceWiring.scala
@@ -16,9 +16,7 @@
 
 package uk.gov.hmrc.apprenticeshiplevy.config
 
-import config.ApplicationConfig
 import uk.gov.hmrc.apprenticeshiplevy.connectors.ETMPConnector
-import uk.gov.hmrc.apprenticeshiplevy.data.LevyDeclarations
 import uk.gov.hmrc.play.audit.http.config.LoadAuditingConfig
 import uk.gov.hmrc.play.audit.http.connector.AuditConnector
 import uk.gov.hmrc.play.auth.microservice.connectors.AuthConnector
@@ -26,8 +24,6 @@ import uk.gov.hmrc.play.config.{AppName, RunMode, ServicesConfig}
 import uk.gov.hmrc.play.http.HttpGet
 import uk.gov.hmrc.play.http.hooks.HttpHook
 import uk.gov.hmrc.play.http.ws._
-
-import scala.concurrent.Future
 
 object WSHttp extends WSGet with WSPut with WSPost with WSDelete with WSPatch with AppName {
   override val hooks: Seq[HttpHook] = NoneRequired
@@ -44,6 +40,5 @@ object MicroserviceAuthConnector extends AuthConnector with ServicesConfig {
 object MicroserviceETMPConnector extends ETMPConnector with ServicesConfig {
 
   override def httpGet: HttpGet = WSHttp
-
-  override val etmpBaseUrl = ApplicationConfig.etmpUrl
+  override val etmpBaseUrl = AppContext.etmpUrl
 }
