b059683f3165d5ba34d74f07b3b38a0cd2be3a8f
Doug Clinton <doug@wiredthing.com>
2016-06-08 11:40:03 +0100

    Added a first pass at a UserInfoController to get list of accounts accessible to the user

--- app/uk/gov/hmrc/apprenticeshiplevy/config/microserviceWiring.scala
+++ app/uk/gov/hmrc/apprenticeshiplevy/config/microserviceWiring.scala
@@ -16,14 +16,18 @@
 
 package uk.gov.hmrc.apprenticeshiplevy.config
 
+import play.api.libs.concurrent.Execution.Implicits.defaultContext
 import uk.gov.hmrc.apprenticeshiplevy.connectors.ETMPConnector
 import uk.gov.hmrc.play.audit.http.config.LoadAuditingConfig
 import uk.gov.hmrc.play.audit.http.connector.AuditConnector
 import uk.gov.hmrc.play.auth.microservice.connectors.AuthConnector
 import uk.gov.hmrc.play.config.{AppName, RunMode, ServicesConfig}
-import uk.gov.hmrc.play.http.HttpGet
+import uk.gov.hmrc.play.frontend.auth.connectors.domain.Authority
 import uk.gov.hmrc.play.http.hooks.HttpHook
 import uk.gov.hmrc.play.http.ws._
+import uk.gov.hmrc.play.http.{HeaderCarrier, HttpGet}
+
+import scala.concurrent.Future
 
 object WSHttp extends WSGet with WSPut with WSPost with WSDelete with WSPatch with AppName {
   override val hooks: Seq[HttpHook] = NoneRequired
@@ -35,10 +39,17 @@ object MicroserviceAuditConnector extends AuditConnector with RunMode {
 
 object MicroserviceAuthConnector extends AuthConnector with ServicesConfig {
   override val authBaseUrl = baseUrl("auth")
+
+  def http: HttpGet = WSHttp
+
+  def currentAuthority(implicit hc: HeaderCarrier): Future[Option[Authority]] = {
+    http.GET[Authority](s"$authBaseUrl/auth/authority").map(Some.apply) // Option return is legacy of previous http library now baked into this class's api
+  }
 }
 
 object MicroserviceETMPConnector extends ETMPConnector with ServicesConfig {
 
   override def httpGet: HttpGet = WSHttp
+
   override val etmpBaseUrl = AppContext.etmpUrl
 }
--- project/MicroServiceBuild.scala
+++ project/MicroServiceBuild.scala
@@ -21,6 +21,7 @@ private object AppDependencies {
 
   private val microserviceBootstrapVersion = "4.2.1"
   private val playAuthVersion = "3.1.0"
+  private val playAuthFrontendVersion = "5.0.0"
   private val playHealthVersion = "1.1.0"
   private val playJsonLoggerVersion = "2.1.1"  
   private val playUrlBindersVersion = "1.0.0"
@@ -29,10 +30,10 @@ private object AppDependencies {
   private val hmrcTestVersion = "1.6.0"
 
   val compile = Seq(
-
     ws,
     "uk.gov.hmrc" %% "microservice-bootstrap" % microserviceBootstrapVersion,
     "uk.gov.hmrc" %% "play-authorisation" % playAuthVersion,
+    "uk.gov.hmrc" %% "play-authorised-frontend" % playAuthFrontendVersion,
     "uk.gov.hmrc" %% "play-health" % playHealthVersion,
     "uk.gov.hmrc" %% "play-url-binders" % playUrlBindersVersion,
     "uk.gov.hmrc" %% "play-config" % playConfigVersion,
