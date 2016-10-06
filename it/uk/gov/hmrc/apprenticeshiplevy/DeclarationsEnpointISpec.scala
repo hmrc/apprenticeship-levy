e99ee9c259f44a4f5c12f6c8096581e34467ddde
Leanne Northrop <leanne.northrop@gmail.com>
2016-10-06 14:03:12 +0100

    DDCNLS-110: Implemented test for valid declarations endpoint call.

--- it/uk/gov/hmrc/apprenticeshiplevy/DeclarationsEnpointISpec.scala
+++ it/uk/gov/hmrc/apprenticeshiplevy/DeclarationsEnpointISpec.scala
@@ -3,6 +3,8 @@ package uk.gov.hmrc.apprenticeshiplevy
 import org.scalatest._
 import org.scalatest.Matchers._
 
+import org.joda.time.{LocalDate, LocalDateTime}
+
 import org.scalacheck.Gen
 
 import play.api.test.{FakeRequest, Helpers, RouteInvokers}
@@ -18,6 +20,7 @@ import com.github.tomakehurst.wiremock.http.Fault
 
 import uk.gov.hmrc.apprenticeshiplevy.util._
 import uk.gov.hmrc.play.test.UnitSpec
+import uk.gov.hmrc.apprenticeshiplevy.data.{LevyDeclaration,PayrollPeriod}
 
 @DoNotDiscover
 class DeclarationsEndpointISpec extends WiremockFunSpec  {
@@ -26,16 +29,25 @@ class DeclarationsEndpointISpec extends WiremockFunSpec  {
     contexts.foreach { case (context) =>
       describe (s"should when calling ${localMicroserviceUrl}$context/epaye/<empref>/declarations") {
         describe (s"with valid paramters") {
-          it (s"return date") {
+          it (s"should return levy declarations") {
             // set up
-            val request = FakeRequest(GET, s"$context/fraction-calculation-date").withHeaders("ACCEPT"->"application/vnd.hmrc.1.0+json")
+            val request = FakeRequest(GET, s"$context/epaye/123%2FAB12345/declarations").withHeaders("ACCEPT"->"application/vnd.hmrc.1.0+json")
 
             // test
             val result = route(request).get
 
             // check
             contentType(result) shouldBe Some("application/json")
-            contentAsJson(result) shouldBe Json.parse(""""2016-06-19"""")
+            val json = contentAsJson(result)
+            (json \ "empref").as[String] shouldBe "123/AB12345"
+            val declarations = (json \ "declarations").as[Array[LevyDeclaration]]
+            declarations should contain atLeastOneOf (LevyDeclaration(12345684,
+                                                                      new LocalDateTime(2016, 10, 15, 16, 5, 23, 123),
+                                                                      Some(new LocalDate(2016, 9, 5))),
+                                                      LevyDeclaration(12345679,
+                                                                      new LocalDateTime(2015, 4, 7, 16, 5, 23, 123),
+                                                                      payrollPeriod=Some(PayrollPeriod("15-16", 12)),
+                                                                      noPaymentForPeriod=Some(true)))
           }
         }
 
