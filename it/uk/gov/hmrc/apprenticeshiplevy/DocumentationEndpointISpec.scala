0007e2336bb493c31d47908a73c9ea7772e9d7f2
Leanne Northrop <leanne.northrop@gmail.com>
2016-10-06 11:44:39 +0100

    DDCNLS-110: Tweaked order of tests so that results read better.

--- it/uk/gov/hmrc/apprenticeshiplevy/DocumentationEndpointISpec.scala
+++ it/uk/gov/hmrc/apprenticeshiplevy/DocumentationEndpointISpec.scala
@@ -30,76 +30,79 @@ class DocumentationEndpointISpec extends WiremockFunSpec  {
     loadString(content)
   }
 
-  describe (s"Calling ${localMicroserviceUrl}/api/documentation/xxx/yyy") {
-    it (s"return 404 when documentation version doesn't exist") {
-      // set up
-      WiremockService.notifier.testInformer = NullInformer.info
-      val urls = for { version <- Gen.choose(Int.MinValue, Int.MaxValue) } yield (s"/api/documentation/${version}/empref")
+  describe (s"API Documentation") {
+    describe (s"when called with valid paramters") {
+      val definitionFile = new File("./public/api/definition.json")
+      val definitionContents = Source.fromFile(definitionFile).getLines.mkString
+      val definitionJson = Json.parse(definitionContents)
+      val versions = (definitionJson \\ "version") map (_.as[String])
+      versions.foreach { case (version) =>
+          describe (s"Calling ${localMicroserviceUrl}/api/documentation/$version") {
+            val endpointNames = (definitionJson \\ "endpoints").map(_ \\ "endpointName").map(_.map(_.as[String].toLowerCase))
+            versions.zip(endpointNames).flatMap { case (version, endpoint) =>
+              endpoint.map(endpointName => (version, endpointName))
+            }.filter(_._1 == version).foreach { case (v, endpointName) =>
+              it (s"/$endpointName should return expected documentation") {
+                // set up
+                val request = FakeRequest(GET, s"/api/documentation/$version/$endpointName")
+                val expectedXml = asXml(asString(s"${endpointName}.xml"))
 
-      forAll(urls) { (url: String) =>
-        val request = FakeRequest(GET, url)
+                // test
+                val documentationResult = route(request).get
+                val httpStatus = status(documentationResult)
+                val xml = asXml(contentAsString(documentationResult))
+                val contenttype = contentType(documentationResult)
 
-        // test
-        val documentationResult = route(request).get
-        val httpStatus = status(documentationResult)
-
-        // check
-        httpStatus shouldBe 404
+                // check
+                httpStatus shouldBe OK
+                contenttype shouldBe Some("application/xml")
+                xml should beXml (expectedXml, true)
+              }
+            }
+          }
       }
     }
 
+    describe (s"when called with invalid paramters") {
+      describe (s"Calling ${localMicroserviceUrl}/api/documentation/<version>/<endpoint-name>") {
+        it (s"return 404 when documentation version doesn't exist") {
+          // set up
+          WiremockService.notifier.testInformer = NullInformer.info
+          val urls = for { version <- Gen.choose(Int.MinValue, Int.MaxValue) } yield (s"/api/documentation/${version}/empref")
 
-    it (s"return 404 when documentation endpoint doesn't exist") {
-      // set up
-      WiremockService.notifier.testInformer = NullInformer.info
-      val endpoints = for { endpoint <- Gen.alphaStr } yield (endpoint)
-
-      forAll(endpoints) { (endpoint: String) =>
-        whenever (!endpoint.isEmpty && endpoint != "/") {
-          val url = s"/api/documentation/1.0/${endpoint}"
-          val request = FakeRequest(GET, url)
-
-          // test
-          val documentationResult = route(request).get
-          val httpStatus = status(documentationResult)
-
-          // check
-          httpStatus shouldBe 404
-        }
-      }
-    }
-  }
-
-  override def run(testName: Option[String], args: Args): Status = {
-    val definitionFile = Play.current.getFile("public/api/definition.json")
-    val definitionContents = Source.fromFile(definitionFile).getLines.mkString
-    val definitionJson = Json.parse(definitionContents)
-    val versions = (definitionJson \\ "version") map (_.as[String])
-    versions.foreach { case (version) =>
-      describe (s"Calling ${localMicroserviceUrl}/api/documentation/$version/") {
-        val endpointNames = (definitionJson \\ "endpoints").map(_ \\ "endpointName").map(_.map(_.as[String].toLowerCase))
-        versions.zip(endpointNames).flatMap { case (version, endpoint) =>
-          endpoint.map(endpointName => (version, endpointName))
-        }.filter(_._1 == version).foreach { case (v, endpointName) =>
-          it (s"/$endpointName should return expected documentation") {
-            // set up
-            val request = FakeRequest(GET, s"/api/documentation/$version/$endpointName")
-            val expectedXml = asXml(asString(s"${endpointName}.xml"))
+          forAll(urls) { (url: String) =>
+            val request = FakeRequest(GET, url)
 
             // test
             val documentationResult = route(request).get
             val httpStatus = status(documentationResult)
-            val xml = asXml(contentAsString(documentationResult))
-            val contenttype = contentType(documentationResult)
 
             // check
-            httpStatus shouldBe OK
-            contenttype shouldBe Some("application/xml")
-            xml should beXml (expectedXml, true)
+            httpStatus shouldBe 404
+          }
+        }
+
+
+        it (s"return 404 when documentation endpoint doesn't exist") {
+          // set up
+          WiremockService.notifier.testInformer = NullInformer.info
+          val endpoints = for { endpoint <- Gen.alphaStr } yield (endpoint)
+
+          forAll(endpoints) { (endpoint: String) =>
+            whenever (!endpoint.isEmpty && endpoint != "/") {
+              val url = s"/api/documentation/1.0/${endpoint}"
+              val request = FakeRequest(GET, url)
+
+              // test
+              val documentationResult = route(request).get
+              val httpStatus = status(documentationResult)
+
+              // check
+              httpStatus shouldBe 404
+            }
           }
         }
       }
     }
-    super.run(testName, args)
   }
 }
