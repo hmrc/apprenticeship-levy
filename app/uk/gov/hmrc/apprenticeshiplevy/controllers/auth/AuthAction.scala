d2606855612f1ca48b3e278ec785e216bb8e8a77
Jay Nabonne <jaynabonne@gmail.com>
2019-12-03 17:14:00 +0000

    DDCNLS-6204b: Work on getting the integration tests to work. WIP. Tests do not pass.

--- app/uk/gov/hmrc/apprenticeshiplevy/controllers/auth/AuthAction.scala
+++ app/uk/gov/hmrc/apprenticeshiplevy/controllers/auth/AuthAction.scala
@@ -16,21 +16,28 @@
 
 package uk.gov.hmrc.apprenticeshiplevy.controllers.auth
 
+import java.io.IOException
+
 import com.google.inject.{ImplementedBy, Inject}
+import org.slf4j.MDC
 import play.api.Mode.Mode
-import play.api.mvc.Results.Unauthorized
+import play.api.http.Status._
+import play.api.libs.json.Json
+import play.api.mvc.Results.{BadRequest, Forbidden, InternalServerError, NotFound, RequestTimeout, ServiceUnavailable, TooManyRequests, Unauthorized}
 import play.api.mvc._
 import play.api.{Configuration, Logger, Play}
 import uk.gov.hmrc.apprenticeshiplevy.config.WSHttp
+import uk.gov.hmrc.apprenticeshiplevy.controllers.AuthError
 import uk.gov.hmrc.auth.core.AuthProvider.PrivilegedApplication
 import uk.gov.hmrc.auth.core._
 import uk.gov.hmrc.auth.core.retrieve.v2.Retrievals
 import uk.gov.hmrc.domain.EmpRef
-import uk.gov.hmrc.http.{CorePost, HeaderCarrier}
+import uk.gov.hmrc.http.{Request => _, _}
 import uk.gov.hmrc.play.HeaderCarrierConverter
 import uk.gov.hmrc.play.config.ServicesConfig
 
 import scala.concurrent.{ExecutionContext, Future}
+import scala.util.Try
 
 class AuthActionImpl @Inject()(val authConnector: AuthConnector)(implicit executionContext: ExecutionContext)
   extends AuthAction with AuthorisedFunctions {
@@ -51,11 +58,123 @@ class AuthActionImpl @Inject()(val authConnector: AuthConnector)(implicit execut
             }
           }
         Future.successful(Right(AuthenticatedRequest(request, payeRef)))
-      case _ => Future.successful(Right(AuthenticatedRequest(request, None)))
-    }.recover {
-      case t: Throwable =>
-        Logger.debug("Debug info - " + t.getMessage)
-        Left(Unauthorized)
+      case _ =>
+        Future.successful(Right(AuthenticatedRequest(request, None)))
+
+    }.recover { case e: Throwable => Left(authErrorHandler(e)) }
+
+  }
+
+  private def extractReason(msg: String): String =
+    Try(if (msg.contains("Response body")) {
+      val str1 = msg.reverse.substring(1).reverse.substring(msg.indexOf("Response body") + 14).trim
+      val m = if (str1.startsWith("{")) str1 else str1.substring(str1.indexOf("{"))
+      Try((Json.parse(m) \ "reason").as[String]) getOrElse ((Json.parse(m) \ "Reason").as[String])
+    } else {msg}) getOrElse(msg)
+
+  private def authErrorHandler(exc: Throwable): Result = {
+    println("============== Exception =================: " + exc.getMessage + " " + exc.getClass)
+    exc match {
+      case e: BadRequestException =>
+        Logger.warn(s"Client ${
+          MDC.get("X-Client-ID")
+        } API error: ${
+          e.getMessage
+        }, API returning BadRequest with code ${
+          SERVICE_UNAVAILABLE
+        }")
+        BadRequest(Json.toJson(AuthError(SERVICE_UNAVAILABLE, "BAD_REQUEST", s"Bad request error: ${
+          extractReason(e.getMessage)
+        }")))
+      case e: IOException =>
+        Logger.error(s"Client ${
+          MDC.get("X-Client-ID")
+        } API error: ${
+          e.getMessage
+        }, API returning ServiceUnavailable with code ${
+          SERVICE_UNAVAILABLE
+        }", e)
+        ServiceUnavailable(Json.toJson(AuthError(SERVICE_UNAVAILABLE, "IO", s"Auth connection error: ${
+          extractReason(e.getMessage)
+        }")))
+      case e: GatewayTimeoutException =>
+        Logger.error(s"Client ${
+          MDC.get("X-Client-ID")
+        } API error: ${
+          e.getMessage
+        }, API returning RequestTimeout with code ${
+          GATEWAY_TIMEOUT
+        }", e)
+        RequestTimeout(Json.toJson(AuthError(REQUEST_TIMEOUT, "GATEWAY_TIMEOUT", s"Auth not responding error: ${
+          extractReason(e.getMessage)
+        }")))
+      case e: NotFoundException =>
+        Logger.warn(s"Client ${
+          MDC.get("X-Client-ID")
+        } API error: ${
+          e.getMessage
+        }, API returning NotFound with code ${
+          NOT_FOUND
+        }")
+        NotFound(Json.toJson(AuthError(NOT_FOUND, "NOT_FOUND", s"Auth endpoint not found: ${
+          extractReason(e.getMessage)
+        }")))
+      case e: Upstream5xxResponse =>
+        Logger.error(s"Client ${
+          MDC.get("X-Client-ID")
+        } API error: ${
+          e.getMessage
+        }, API returning ServiceUnavailable with code ${
+          e.reportAs
+        }", e)
+        ServiceUnavailable(Json.toJson(AuthError(e.reportAs, "BACKEND_FAILURE", s"Auth 5xx error: ${
+          extractReason(e.getMessage)
+        }")))
+      case e: Upstream4xxResponse =>
+        Logger.warn(s"Client ${
+          MDC.get("X-Client-ID")
+        } API error: ${
+          e.getMessage
+        } with ${
+          e.upstreamResponseCode
+        }, API returning code ${
+          e.reportAs
+        }")
+        e.upstreamResponseCode match {
+          case FORBIDDEN => Forbidden(Json.toJson(AuthError(e.reportAs, "FORBIDDEN", s"Auth forbidden error: ${
+            extractReason(e.getMessage)
+          }")))
+          case UNAUTHORIZED => Unauthorized(Json.toJson(AuthError(e.reportAs, "UNAUTHORIZED", s"Auth unauthorised error: ${
+            extractReason(e.getMessage)
+          }")))
+          case TOO_MANY_REQUESTS => TooManyRequests(Json.toJson(AuthError(TOO_MANY_REQUESTS, "TOO_MANY_REQUESTS", s"Auth too many requests: ${
+            extractReason(e.getMessage)
+          }")))
+          case REQUEST_TIMEOUT => RequestTimeout(Json.toJson(AuthError(REQUEST_TIMEOUT, "TIMEOUT", s"Auth not responding error: ${
+            extractReason(e.getMessage)
+          }")))
+          case _ => ServiceUnavailable(Json.toJson(AuthError(e.reportAs, "OTHER", s"Auth 4xx error: ${
+            extractReason(e.getMessage)
+          }")))
+        }
+      case e: _root_.uk.gov.hmrc.http.JsValidationException =>
+        Logger.error(s"Client ${
+          MDC.get("X-Client-ID")
+        } API error: ${
+          e.getMessage
+        }, API returning Unauthorized 498, WRONG_TOKEN", e)
+        Unauthorized(Json.toJson(AuthError(498, "WRONG_TOKEN", s"Auth unauthorised error: OAUTH 2 User Token Required not TOTP")))
+      case e: Throwable =>
+        Logger.error(s"Client ${
+          MDC.get("X-Client-ID")
+        } API error: ${
+          e.getMessage
+        }, API returning code ${
+          INTERNAL_SERVER_ERROR
+        }", e)
+        InternalServerError(Json.toJson(AuthError(INTERNAL_SERVER_ERROR, "API", s"API or Auth internal server error: ${
+          extractReason(e.getMessage)
+        }")))
     }
   }
 
--- it/uk/gov/hmrc/apprenticeshiplevy/RootEndpointISpec.scala
+++ it/uk/gov/hmrc/apprenticeshiplevy/RootEndpointISpec.scala
@@ -27,14 +27,15 @@ class RootEndpointISpec extends WiremockFunSpec with ConfiguredServer {
         contentAsString(result) shouldBe """{"code":"AUTH_ERROR_WRONG_TOKEN","message":"Auth unauthorised error: OAUTH 2 User Token Required not TOTP"}"""
     }
 
-    val contexts = Seq("/sandbox", "")
+    val contexts = Seq("")
     contexts.foreach { case (context) =>
       describe (s"should when calling ${localMicroserviceUrl}$context/") {
         describe (s"and backend systems not failing") {
           it (s"should return links for each empref") {
             // set up
-            val response = fileToStr("./it/resources/data/input/mapping_body")
-            stubFor(get(urlEqualTo("/auth/authority")).withId(uuid).willReturn(aResponse().withBody(response)))
+//            val response = fileToStr("./it/resources/data/input/mapping_body")
+            val response = """{"allEnrolments": []}"""
+            stubFor(post(urlEqualTo("/auth/authorise")).willReturn(aResponse().withBody(response)))
             val request = FakeRequest(GET, s"$context/").withHeaders(standardDesHeaders: _*)
 
             // test
@@ -44,6 +45,7 @@ class RootEndpointISpec extends WiremockFunSpec with ConfiguredServer {
             status(result) shouldBe OK
             contentType(result) shouldBe Some("application/hal+json")
             val json = contentAsJson(result)
+            println("json result is " + json.toString)
             (json \ "_links" \ "self" \ "href").as[String] shouldBe "/"
             (json \ "_links" \ "123/AB12345" \ "href").as[String] shouldBe "/epaye/123%2FAB12345"
           }
@@ -52,7 +54,7 @@ class RootEndpointISpec extends WiremockFunSpec with ConfiguredServer {
         describe ("when errors occur") {
           it (s"should return 401 when Auth returns 401") {
             // set up
-            stubFor(get(urlEqualTo("/auth/authority")).withId(uuid).willReturn(aResponse().withStatus(401).withStatusMessage("Not authorised.")))
+            stubFor(post(urlEqualTo("/auth/authorise")).willReturn(aResponse().withStatus(401).withStatusMessage("Not authorised.")))
             val request = FakeRequest(GET, s"$context/").withHeaders(standardDesHeaders: _*)
 
             // test
@@ -61,12 +63,12 @@ class RootEndpointISpec extends WiremockFunSpec with ConfiguredServer {
             // check
             status(result) shouldBe 401
             contentType(result) shouldBe Some("application/json")
-            contentAsJson(result) shouldBe Json.parse("""{"code":"AUTH_ERROR_UNAUTHORIZED","message":"Auth unauthorised error: GET of 'http://localhost:8080/auth/authority' returned 401. Response body: ''"}""")
+            contentAsJson(result) shouldBe Json.parse("""{"code":"AUTH_ERROR_UNAUTHORIZED","message":"Auth unauthorised error: POST of 'http://localhost:8080/auth/authorise' returned 401. Response body: ''"}""")
           }
 
           it (s"should return 403 when Auth returns 403") {
             // set up
-            stubFor(get(urlEqualTo("/auth/authority")).withId(uuid).willReturn(aResponse().withStatus(403).withStatusMessage("Forbidden.")))
+            stubFor(post(urlEqualTo("/auth/authorise")).willReturn(aResponse().withStatus(403).withStatusMessage("Forbidden.")))
             val request = FakeRequest(GET, s"$context/").withHeaders(standardDesHeaders: _*)
 
             // test
@@ -75,12 +77,12 @@ class RootEndpointISpec extends WiremockFunSpec with ConfiguredServer {
             // check
             status(result) shouldBe 403
             contentType(result) shouldBe Some("application/json")
-            contentAsJson(result) shouldBe Json.parse("""{"code":"AUTH_ERROR_FORBIDDEN","message":"Auth forbidden error: GET of 'http://localhost:8080/auth/authority' returned 403. Response body: ''"}""")
+            contentAsJson(result) shouldBe Json.parse("""{"code":"AUTH_ERROR_FORBIDDEN","message":"Auth forbidden error: POST of 'http://localhost:8080/auth/authorise' returned 403. Response body: ''"}""")
           }
 
           it (s"HTTP 404") {
             // set up
-            stubFor(get(urlEqualTo("/auth/authority")).withId(uuid).willReturn(aResponse().withStatus(404).withStatusMessage("Not found.")))
+            stubFor(post(urlEqualTo("/auth/authorise")).willReturn(aResponse().withStatus(404).withStatusMessage("Not found.")))
             val request = FakeRequest(GET, s"$context/").withHeaders(standardDesHeaders: _*)
 
             // test
@@ -89,14 +91,14 @@ class RootEndpointISpec extends WiremockFunSpec with ConfiguredServer {
             // check
             status(result) shouldBe 404
             contentType(result) shouldBe Some("application/json")
-            contentAsJson(result) shouldBe Json.parse("""{"code":"AUTH_ERROR_NOT_FOUND","message":"Auth endpoint not found: GET of 'http://localhost:8080/auth/authority' returned 404 (Not Found). Response body: ''"}""")
+            contentAsJson(result) shouldBe Json.parse("""{"code":"AUTH_ERROR_NOT_FOUND","message":"Auth endpoint not found: POST of 'http://localhost:8080/auth/authorise' returned 404 (Not Found). Response body: ''"}""")
           }
         }
 
         describe ("when backend systems failing") {
           it (s"should return http status 503 when connection closed") {
             // set up
-            stubFor(get(urlEqualTo("/auth/authority")).withId(uuid).willReturn(aResponse().withFault(Fault.MALFORMED_RESPONSE_CHUNK)))
+            stubFor(post(urlEqualTo("/auth/authorise")).willReturn(aResponse().withFault(Fault.MALFORMED_RESPONSE_CHUNK)))
             val request = FakeRequest(GET, s"$context/").withHeaders(standardDesHeaders: _*)
 
             // test
@@ -110,7 +112,7 @@ class RootEndpointISpec extends WiremockFunSpec with ConfiguredServer {
 
           it (s"should return 503 when returning empty response and connection closed") {
             // set up
-            stubFor(get(urlEqualTo("/auth/authority")).withId(uuid).willReturn(aResponse().withFault(Fault.EMPTY_RESPONSE)))
+            stubFor(post(urlEqualTo("/auth/authorise")).willReturn(aResponse().withFault(Fault.EMPTY_RESPONSE)))
             val request = FakeRequest(GET, s"$context/").withHeaders(standardDesHeaders: _*)
 
             // test
@@ -124,7 +126,7 @@ class RootEndpointISpec extends WiremockFunSpec with ConfiguredServer {
 
           it (s"should return 408 when timed out") {
             // set up
-            stubFor(get(urlEqualTo("/auth/authority")).withId(uuid).willReturn(aResponse().withStatus(200).withFixedDelay(1000*60)))
+            stubFor(post(urlEqualTo("/auth/authorise")).willReturn(aResponse().withStatus(200).withFixedDelay(1000*60)))
             val request = FakeRequest(GET, s"$context/").withHeaders(standardDesHeaders: _*)
 
             // test
@@ -133,12 +135,12 @@ class RootEndpointISpec extends WiremockFunSpec with ConfiguredServer {
             // check
             status(result) shouldBe 408
             contentType(result) shouldBe Some("application/json")
-            contentAsJson(result) shouldBe Json.parse("""{"code":"AUTH_ERROR_GATEWAY_TIMEOUT","message":"Auth not responding error: GET of 'http://localhost:8080/auth/authority' timed out with message 'Request timeout to localhost/127.0.0.1:8080 after 500 ms'"}""")
+            contentAsJson(result) shouldBe Json.parse("""{"code":"AUTH_ERROR_GATEWAY_TIMEOUT","message":"Auth not responding error: POST of 'http://localhost:8080/auth/authorise' timed out with message 'Request timeout to localhost/127.0.0.1:8080 after 500 ms'"}""")
           }
 
           it (s"should return 503 when Auth returns 500") {
             // set up
-            stubFor(get(urlEqualTo("/auth/authority")).withId(uuid).willReturn(aResponse().withStatus(500).withStatusMessage("Internal server error")))
+            stubFor(post(urlEqualTo("/auth/authorise")).willReturn(aResponse().withStatus(500).withStatusMessage("Internal server error")))
             val request = FakeRequest(GET, s"$context/").withHeaders(standardDesHeaders: _*)
 
             // test
@@ -147,12 +149,12 @@ class RootEndpointISpec extends WiremockFunSpec with ConfiguredServer {
             // check
             status(result) shouldBe 503
             contentType(result) shouldBe Some("application/json")
-            contentAsJson(result) shouldBe Json.parse("""{"code":"AUTH_ERROR_BACKEND_FAILURE","message":"Auth 5xx error: GET of 'http://localhost:8080/auth/authority' returned 500. Response body: ''"}""")
+            contentAsJson(result) shouldBe Json.parse("""{"code":"AUTH_ERROR_BACKEND_FAILURE","message":"Auth 5xx error: POST of 'http://localhost:8080/auth/authorise' returned 500. Response body: ''"}""")
           }
 
           it (s"should return 503 when Auth returns 503") {
             // set up
-            stubFor(get(urlEqualTo("/auth/authority")).withId(uuid).willReturn(aResponse().withStatus(503).withStatusMessage("Backend systems failing")))
+            stubFor(post(urlEqualTo("/auth/authorise")).willReturn(aResponse().withStatus(503).withStatusMessage("Backend systems failing")))
             val request = FakeRequest(GET, s"$context/").withHeaders(standardDesHeaders: _*)
 
             // test
@@ -161,12 +163,12 @@ class RootEndpointISpec extends WiremockFunSpec with ConfiguredServer {
             // check
             status(result) shouldBe 503
             contentType(result) shouldBe Some("application/json")
-            contentAsJson(result) shouldBe Json.parse("""{"code":"AUTH_ERROR_BACKEND_FAILURE","message":"Auth 5xx error: GET of 'http://localhost:8080/auth/authority' returned 503. Response body: ''"}""")
+            contentAsJson(result) shouldBe Json.parse("""{"code":"AUTH_ERROR_BACKEND_FAILURE","message":"Auth 5xx error: POST of 'http://localhost:8080/auth/authorise' returned 503. Response body: ''"}""")
           }
 
           it (s"should return http status 429 when Auth HTTP 429") {
             // set up
-            stubFor(get(urlEqualTo("/auth/authority")).withId(uuid).willReturn(aResponse().withStatus(429).withBody("""{"reason" : "Drowning in requests"}""")))
+            stubFor(post(urlEqualTo("/auth/authorise")).willReturn(aResponse().withStatus(429).withBody("""{"reason" : "Drowning in requests"}""")))
             val request = FakeRequest(GET, s"$context/").withHeaders(standardDesHeaders: _*)
 
             // test
@@ -180,7 +182,7 @@ class RootEndpointISpec extends WiremockFunSpec with ConfiguredServer {
 
           it (s"should return http status 503 when Auth HTTP 409") {
             // set up
-            stubFor(get(urlEqualTo("/auth/authority")).withId(uuid).willReturn(aResponse().withStatus(411).withBody("""{"reason" : "Some Auth 411 error"}""")))
+            stubFor(post(urlEqualTo("/auth/authorise")).willReturn(aResponse().withStatus(411).withBody("""{"reason" : "Some Auth 411 error"}""")))
             val request = FakeRequest(GET, s"$context/").withHeaders(standardDesHeaders: _*)
 
             // test
@@ -194,7 +196,7 @@ class RootEndpointISpec extends WiremockFunSpec with ConfiguredServer {
 
           it (s"should return http status 408 when Auth HTTP 408") {
             // set up
-            stubFor(get(urlEqualTo("/auth/authority")).withId(uuid).willReturn(aResponse().withStatus(408).withBody("""{"reason" : "Not responding"}""")))
+            stubFor(post(urlEqualTo("/auth/authorise")).willReturn(aResponse().withStatus(408).withBody("""{"reason" : "Not responding"}""")))
             val request = FakeRequest(GET, s"$context/").withHeaders(standardDesHeaders: _*)
 
             // test
@@ -208,7 +210,7 @@ class RootEndpointISpec extends WiremockFunSpec with ConfiguredServer {
 
           it (s"should return http status 400 when Auth HTTP 400") {
             // set up
-            stubFor(get(urlEqualTo("/auth/authority")).withId(uuid).willReturn(aResponse().withStatus(400).withBody("""{"reason" : "Not responding"}""")))
+            stubFor(post(urlEqualTo("/auth/authorise")).willReturn(aResponse().withStatus(400).withBody("""{"reason" : "Not responding"}""")))
             val request = FakeRequest(GET, s"$context/").withHeaders(standardDesHeaders: _*)
 
             // test
