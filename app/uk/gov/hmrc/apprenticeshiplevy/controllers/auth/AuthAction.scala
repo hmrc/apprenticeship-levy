87b7bce5d4be9d6f060a4c734bfc2d30efc90ec6
danewatts <danewatts97@gmail.com>
2020-03-25 12:55:52 +0000

    Handled authorisation related exceptions with 401

--- app/uk/gov/hmrc/apprenticeshiplevy/controllers/auth/AuthAction.scala
+++ app/uk/gov/hmrc/apprenticeshiplevy/controllers/auth/AuthAction.scala
@@ -48,7 +48,7 @@ class AuthActionImpl @Inject()(val authConnector: AuthConnector)(implicit execut
   override protected def refine[A](request: Request[A]): Future[Either[Result, AuthenticatedRequest[A]]] = {
     implicit val hc: HeaderCarrier = HeaderCarrierConverter.fromHeadersAndSession(request.headers, None)
 
-    authorised( ).retrieve(Retrievals.allEnrolments) {
+    authorised().retrieve(Retrievals.allEnrolments) {
       case Enrolments(enrolments) =>
         val payeRef: Option[EmpRef] = enrolments.find(_.key == "IR-PAYE")
           .flatMap { enrolment =>
@@ -143,6 +143,11 @@ private object ErrorHandler {
       case e: SessionRecordNotFound =>
         logWarningAboutException(e, UNAUTHORIZED, "Unauthorized with code")
         Unauthorized(Json.toJson(AuthError(UNAUTHORIZED, "UNAUTHORIZED", s"No active session error: ${
+          extractReason(e.getMessage)
+        }")))
+      case e: AuthorisationException =>
+        logWarningAboutException(e, UNAUTHORIZED, "Unauthorized with code")
+        Unauthorized(Json.toJson(AuthError(UNAUTHORIZED, "UNAUTHORIZED", s"${
           extractReason(e.getMessage)
         }")))
       case e: BadRequestException =>
