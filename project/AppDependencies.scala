d2a387358b877d12b562063a35f495d5a3e86873
Daniel P. Doyle <3020488+TheDanielDoyle@users.noreply.github.com>
2022-06-14 16:22:52 +0100

    DDCNL-6048 - As per HMRC Bootstrap documentation, i've brought in bootstrap-test-play-28 as it contains correct versions of some of the other dependencies such as scalatest. I've also gotten the IT and Unit tests compiling.

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -11,34 +11,31 @@ object AppDependencies {
     "uk.gov.hmrc" %% "play-hal"                  % "3.1.0-play-28",
     "uk.gov.hmrc" %% "domain"                    % "8.1.0-play-28",
     "uk.gov.hmrc" %% "time"                      % "3.25.0"
-
   )
 
   lazy val test: Seq[ModuleID] = Seq(
-    "org.scalatest"          %% "scalatest"                    % "3.2.12",
-    "com.typesafe.play"      %% "play-test"                    % PlayVersion.current,
-    "org.pegdown"             % "pegdown"                      % "1.6.0",
-    "org.scalamock"          %% "scalamock-scalatest-support"  % "3.6.0",
-    "org.mockito"            %  "mockito-core"                 % "4.6.1",
-    "org.scalatestplus.play" %% "scalatestplus-play"           % "5.1.0",
-    "com.github.tomakehurst" %  "wiremock-jre8"                % "2.27.2"
+    "uk.gov.hmrc"            %% "bootstrap-test-play-28"      % "5.24.0",
+    "com.typesafe.play"      %% "play-test"                   % PlayVersion.current,
+    "org.pegdown"             % "pegdown"                     % "1.6.0",
+    "org.scalamock"          %% "scalamock-scalatest-support" % "3.6.0",
+    "org.mockito"            %  "mockito-core"                % "4.6.1",
+    "com.github.tomakehurst" %  "wiremock-jre8"               % "2.27.2"
   ).map(_ % "test")
 
   lazy val integrationTest: Seq[ModuleID] = Seq(
-    "org.scalatest"           %% "scalatest"          % "3.2.12",
-    "org.pegdown"              % "pegdown"            % "1.6.0",
-    "com.typesafe.play"       %% "play-test"          % PlayVersion.current,
-    "com.github.tomakehurst"   % "wiremock-jre8"      % "2.27.2",
-    "org.scala-lang.modules"  %% "scala-xml"          % "2.1.0",
-    "com.github.andyglow"     %% "scala-xml-diff"     % "3.0.0",
-    "org.scalatestplus"       %% "scalacheck-1-14"    % "3.2.2.0",
-    "org.scalatestplus.play"  %% "scalatestplus-play" % "5.1.0"
+    "uk.gov.hmrc"            %% "bootstrap-test-play-28" % "5.24.0",
+    "org.pegdown"             % "pegdown"                % "1.6.0",
+    "com.typesafe.play"      %% "play-test"              % PlayVersion.current,
+    "com.github.tomakehurst"  % "wiremock-jre8"          % "2.27.2",
+    "org.scala-lang.modules" %% "scala-xml"              % "2.1.0",
+    "com.github.andyglow"    %% "scala-xml-diff"         % "3.0.0",
+    "org.scalatestplus"      %% "scalacheck-1-14"        % "3.2.2.0"
   ).map(_ % "test,it")
 
   lazy val acceptanceTest: Seq[ModuleID] = Seq(
-    "org.scalatest" %% "scalatest"    % "3.2.12",
-    "org.pegdown"    % "pegdown"      % "1.6.0",
-    "org.scalaj"    %% "scalaj-http"  % "2.4.2"
+    "uk.gov.hmrc"   %% "bootstrap-test-play-28" % "5.24.0",
+    "org.pegdown"    % "pegdown"                % "1.6.0",
+    "org.scalaj"    %% "scalaj-http"            % "2.4.2",
   ).map(_ % "test, ac")
 
   val generateApiTask: Seq[ModuleID] = {
