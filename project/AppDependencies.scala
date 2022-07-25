4851e277c3ca5f12adbcb229d027ca7539e34629
Nick Ward <ward.nickjames@gmail.com>
2022-07-25 14:31:00 +0100

    [DDCNL 5919] Update dependencies (#180)
    * Update play bootstrap
    
    * Update play hmrc api
    
    * Update bootstrap play in acceptanceTest
    
    * Update sbt version
    
    * Update sbt plugins

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,15 +6,15 @@ object AppDependencies {
 
   val compile: Seq[ModuleID] = Seq(
     ws,
-    "uk.gov.hmrc" %% "bootstrap-backend-play-28" % "5.24.0",
-    "uk.gov.hmrc" %% "play-hmrc-api"             % "6.4.0-play-28",
+    "uk.gov.hmrc" %% "bootstrap-backend-play-28" % "6.3.0",
+    "uk.gov.hmrc" %% "play-hmrc-api"             % "7.0.0-play-28",
     "uk.gov.hmrc" %% "play-hal"                  % "3.1.0-play-28",
     "uk.gov.hmrc" %% "domain"                    % "8.1.0-play-28",
     "uk.gov.hmrc" %% "time"                      % "3.25.0"
   )
 
   lazy val test: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc"            %% "bootstrap-test-play-28"      % "5.24.0",
+    "uk.gov.hmrc"            %% "bootstrap-test-play-28"      % "6.3.0",
     "com.typesafe.play"      %% "play-test"                   % PlayVersion.current,
     "org.pegdown"             % "pegdown"                     % "1.6.0",
     "org.scalamock"          %% "scalamock-scalatest-support" % "3.6.0",
@@ -33,7 +33,7 @@ object AppDependencies {
   ).map(_ % "test,it")
 
   lazy val acceptanceTest: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc"   %% "bootstrap-test-play-28" % "5.24.0",
+    "uk.gov.hmrc"   %% "bootstrap-test-play-28" % "6.3.0",
     "org.pegdown"    % "pegdown"                % "1.6.0",
     "org.scalaj"    %% "scalaj-http"            % "2.4.2",
   ).map(_ % "test, ac")
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -2,7 +2,7 @@ resolvers += "Typesafe Releases" at "https://repo.typesafe.com/typesafe/releases
 resolvers += MavenRepository("HMRC-open-artefacts-maven2", "https://open.artefacts.tax.service.gov.uk/maven2")
 resolvers += Resolver.url("HMRC-open-artefacts-ivy", url("https://open.artefacts.tax.service.gov.uk/ivy2"))(Resolver.ivyStylePatterns)
 
-addSbtPlugin("uk.gov.hmrc" % "sbt-auto-build" % "3.0.0")
+addSbtPlugin("uk.gov.hmrc" % "sbt-auto-build" % "3.7.0")
 
 addSbtPlugin("uk.gov.hmrc" % "sbt-git-versioning" % "2.4.0")
 
@@ -10,8 +10,8 @@ addSbtPlugin("uk.gov.hmrc" % "sbt-artifactory" % "2.0.0")
 
 addSbtPlugin("uk.gov.hmrc" % "sbt-distributables" % "2.1.0")
 
-addSbtPlugin("com.typesafe.play" % "sbt-plugin" % "2.8.8")
+addSbtPlugin("com.typesafe.play" % "sbt-plugin" % "2.8.16")
 
 addSbtPlugin("com.orrsella" % "sbt-sublime" % "1.1.2")
 
-addSbtPlugin("org.scoverage" % "sbt-scoverage" % "1.5.1")
+addSbtPlugin("org.scoverage" % "sbt-scoverage" % "2.0.0")
