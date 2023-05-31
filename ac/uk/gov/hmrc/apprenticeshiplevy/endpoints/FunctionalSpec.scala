f9d51cada4c8a0bc1632d7c3f861afa24b740491
Mohammad Abdulhadi <mohammad.abdulhadi@testdrivensolutions.co.uk>
2023-05-31 15:57:52 +0100

    Fixed the failing integration test in apprenticeship-levy

--- ac/uk/gov/hmrc/apprenticeshiplevy/endpoints/FunctionalSpec.scala
+++ ac/uk/gov/hmrc/apprenticeshiplevy/endpoints/FunctionalSpec.scala
@@ -1,20 +1,30 @@
 package uk.gov.hmrc.apprenticeshiplevy.endpoints
 
 import java.io.File
-
 import org.scalatest._
 import uk.gov.hmrc.apprenticeshiplevy.config.Config
 
 import scala.io.Source
+import scala.util.Using
 
 trait FunctionalSpec extends FunSpec {
-  val url = Config.rootUrl
-  val dir = Config.resourceDir
-  val contexts = Config.contexts
+  val url: String = Config.rootUrl
+  val dir: String = Config.resourceDir
+  val contexts: Seq[(String, String)] = Config.contexts
 
-  def standardHeaders(implicit environment: String): Seq[(String,String)] = Seq(("Accept"->"application/vnd.hmrc.1.0+json"),
-                                                                            ("Authorization"->s"""Bearer ${System.getProperty("bearer.token."+System.getProperty("environment", "local").toLowerCase)}"""))
+  def standardHeaders(implicit environment: String): Seq[(String,String)] = Seq("Accept" -> "application/vnd.hmrc.1.0+json",
+                                                                            "Authorization" -> s"""Bearer ${System.getProperty("bearer.token." + System.getProperty("environment", "local").toLowerCase)}""")
 
-  def fileToStr(filename: String): String = Source.fromFile(new File(s"$filename")).getLines.mkString("\n")
-  def fileToStr(file: File): String = Source.fromFile(file).getLines.mkString("\n")
+  def fileToStr(filename: String): String = {
+    Using(Source.fromFile(new File(s"$filename"))) {
+      bufferedSource => bufferedSource.getLines.mkString("\n")
+    }
+  }.get
+
+  def fileToStr(file: File): String = {
+    Using(Source.fromFile(file)) {
+      bufferedSource => bufferedSource.getLines.mkString("\n")
+    }
+  }.get
+
 }
