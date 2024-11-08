e0ed8c805bfab5a087de74c2a5075d3e9c5b2c44
brian-digital <29196549+brian-digital@users.noreply.github.com>
2024-11-08 09:42:50 +0000

    DDCNL-9448: remove unnecessary dependency

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -17,7 +17,6 @@ object AppDependencies {
   lazy val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"            %% s"bootstrap-test-$playVersion"      % bootstrapVersion,
     "org.pegdown"             % "pegdown"                           % "1.6.0",
-//    "org.mockito"             % "mockito-core"                      % "5.11.0",
     "org.scala-lang.modules" %% "scala-xml"                         % "2.3.0",
     "com.github.andyglow"    %% "scala-xml-diff"                    % "3.0.1",
     "org.scalatestplus"      %% "scalacheck-1-17"                   % "3.2.18.0",
