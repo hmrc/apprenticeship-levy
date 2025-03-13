fccfa852f883e84c773c5677b77867a0b3bb5e12
Vincent Vinnicombe <5462800+vinnicombe@users.noreply.github.com>
2025-03-13 16:24:11 +0000

    DDCNL-10165: Plugs

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -10,14 +10,14 @@ object AppDependencies {
   val compile: Seq[ModuleID] = Seq(
     ws,
     "uk.gov.hmrc" %% s"bootstrap-backend-$playVersion" % bootstrapVersion,
-    "uk.gov.hmrc" %%  s"play-hal-$playVersion"         % "4.1.0",
+    "uk.gov.hmrc" %% s"play-hal-$playVersion"          % "4.1.0",
     "uk.gov.hmrc" %% s"domain-$playVersion"            % "10.0.0"
   )
 
   lazy val test: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc"            %% s"bootstrap-test-$playVersion"      % bootstrapVersion,
-    "org.scalatestplus"      %% "scalacheck-1-18"                   % "3.2.19.0",
-    "org.scalaj"             %% "scalaj-http"                       % "2.4.2" cross CrossVersion.for3Use2_13
+    "uk.gov.hmrc"       %% s"bootstrap-test-$playVersion" % bootstrapVersion,
+    "org.scalatestplus" %% "scalacheck-1-18"              % "3.2.19.0",
+    "org.scalaj"        %% "scalaj-http"                  % "2.4.2" cross CrossVersion.for3Use2_13
   ).map(_ % Test)
 
   val generateApiTask: Seq[ModuleID] = {
